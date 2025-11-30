657408958f1622d05506cbe61675a1f9ffcfc217
stchan1981 <handson.chan@gmail.com>
2025-11-30 21:09:36 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
     name: 'proxy-node',
     script: 'index.js',
     instances: 1,
-    autorestart: true,  // 崩溃自动重启
+    autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
-      PORT: 3001,
+      PORT: process.env.PORT || 3002,  // 动态从 env 取，默认 3002
       UUID: process.env.UUID || 'd5646919-1638-4dc6-9799-d795595c6b65',
       ARGO_AUTH: process.env.ARGO_AUTH || ''
     },
